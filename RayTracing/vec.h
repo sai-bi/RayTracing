ab2fc594947b2757f3e6d1afeb1872f86d6b23ba
Bi Sai <soundsilencebisai@gmail.com>
2014-10-03 22:32:14 +0800

    add: class Vec

---
+++ RayTracing/vec.h
@@ -0,0 +1,15 @@
+#ifndef VEC_H
+#define VEC_H
+
+class Vector3D{
+public:
+    double x_;
+    double y_;
+    double z_;
+    
+    Vector3D(){ };
+    Vector3D(double X, double Y, double Z){x = }
+
+};
+ 
+#endif // VE
